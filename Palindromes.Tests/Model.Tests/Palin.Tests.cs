201f3cb42f8a4eb387a278e62bec1a72a23ef410
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-12 09:18:32 -0700

    add Testing framwork and commit before first test
    Co-authored-by: Seung Lee <shl7@uw.edu>

---
+++ Palindromes.Tests/Model.Tests/Palin.Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindromes;
+
+namespace Palindromes.Tests
+{
+  [TestClass]
+  public class PalinTests
+  {
+    [TestMethod]
+    public void IsPalindrome_HasValue_True()
+    {
+
+    }
+  }
+}
---
+++ Palindromes/Models/Palin.cs
@@ -0,0 +1,7 @@
+namespace Palindromes
+{
+  public class Palin
+  {
+    
+  }
+}

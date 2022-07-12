43cc5b8d5d99ad5e58656a0a8dbb6c20ae74ed83
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-12 15:20:03 -0700

    add polish and refactoring
    Co-authored-by: Seung Lee <shl7@uw.edu>

--- Palindromes/Models/Palin.cs
+++ Palindromes/Models/Palin.cs
@@ -1,27 +1,17 @@
-using System;
-
 namespace Palindromes
 {
   public class Palin
   {
     public bool IsPalindrome(string word)
     { 
-      if (word[word.Length - 1] == word[0]) 
+      string[] reverseArray = new string[word.Length];
+      for (int i = 0; i <= word.Length - 1 ; i++) 
       {
-        string[] reverseArray = new string[word.Length];
-        // word = "abcd"
-        for (int i = 0; i <= word.Length - 1 ; i++) 
-        {
-          int x = i;
-          int y = word.Length - (1 + i);
-          reverseArray[x] = word[y].ToString();
-        }
-        return string.Join("", reverseArray) == word; 
+        int x = i;
+        int y = word.Length - (1 + i);
+        reverseArray[x] = word[y].ToString();
       }
-      else 
-      {
-        return false;
-      }
+      return string.Join("", reverseArray) == word; 
     }
 
     public bool IsPalindrome(int number)
@@ -30,16 +20,3 @@ namespace Palindromes
     }
   }
 } 
-
-/*
-give input into array.reverse
-compare reverse array to original with bool
-
-put input into array
-use array.split to split every element in array 
-throw new array in for loop with length as conditional
-
-
-0 1 2 3 4 5
-1 2 3 3 2 1
-*/
